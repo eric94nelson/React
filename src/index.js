4af347f28b60d12f80fa643e249f46afeae8ff4f
eric94nelson <57499024+eric94nelson@users.noreply.github.com>
2019-12-03 11:15:40 -0600

    React Intro
    Intro to React

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const fName = "Eric";
+const lName = "Nelson";
+const num = 5;
+ReactDOM.render(
+  <div>
+    <h1>Hello {fName + " " + lName}!</h1>
+    <p>Your lucky number is {num}</p>
+  </div>,
+  document.getElementById("root")
+);
